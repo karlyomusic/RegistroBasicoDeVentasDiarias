b9161f1d9c818c42e1ede704cfc5d9324590f004
Karla Paola Osio Amaya <osiokarla@gmail.com>
2026-02-27 14:09:01 -0500

    doc: se agregó el try y except al proyecto

--- main.py
+++ main.py
@@ -6,40 +6,46 @@ try:
 
     nombre = input("Ingrese su nombre: ")
     apellido = input("Ingrese su apellido: ")
+except:
+    print("Error: Ingrese su nombre y apellido")
+try:
     precio = float(input("Ingrese el precio: "))
     productos = float(input("Ingrese la cantidad de productos: "))
-
-    cliente = input("Ingrese si tiene membresia si/no: ").lower() == "no"
-
-
-
-    es_vip = bool()
-
-    if cliente == "si":
-        es_vip=True
-    else:
-        es_vip=False    
-
-
-    subtotal = precio * productos
-
-    if(es_vip):
-        descuento = subtotal*0.10
-        print("Tiene acceso a descuento") 
-    else:
-        descuento = 0
-        print("No tiene acceso a descuento")
-
-    total = subtotal - descuento
-
-    print("-"*36)
-    print("    Resumen de la compra   ")
-    print("-"*36)
-
-    print(nombre, apellido)
-    print(subtotal)
-    if(cliente == "si"):
-        print(descuento)
-    print(total)
 except:
-    print("Error: Ingrese ")
+    print("Ingrese un número")
+
+try:
+    cliente = input("Ingrese si tiene membresia si/no: ").lower() == "no"
+except:
+    print("Error: Ingrese un si o un no")
+
+
+
+es_vip = bool()
+
+if cliente == "si":
+    es_vip=True
+else:
+    es_vip=False    
+
+
+subtotal = precio * productos
+
+if(es_vip):
+    descuento = subtotal*0.10
+    print("Tiene acceso a descuento") 
+else:
+    descuento = 0
+    print("No tiene acceso a descuento")
+
+total = subtotal - descuento
+
+print("-"*36)
+print("    Resumen de la compra   ")
+print("-"*36)
+
+print(nombre, apellido)
+print(subtotal)
+if(cliente == "si"):
+    print(descuento)
+print(total)
