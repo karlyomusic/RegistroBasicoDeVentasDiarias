eb13e2ca055dcd5b51cb4adee54fb147df3f3850
Karla Paola Osio Amaya <osiokarla@gmail.com>
2026-02-27 13:56:42 -0500

    doc: Se registró un cambio en el codigo

--- main.py
+++ main.py
@@ -2,30 +2,44 @@ print("-"*36)
 print ("    Registro de compra    ")
 print("-"*36)
 
+try:
 
-nombre = input("Ingrese su nombre: ")
-apellido = input("Ingrese su apellido: ")
-precio = float(input("Ingrese el precio: "))
-productos = float(input("Ingrese la cantidad de productos: "))
+    nombre = input("Ingrese su nombre: ")
+    apellido = input("Ingrese su apellido: ")
+    precio = float(input("Ingrese el precio: "))
+    productos = float(input("Ingrese la cantidad de productos: "))
 
-cliente = input("Ingrese si tiene membresia si/no: ").lower() == "no"
-subtotal = precio * productos
-es_vip = True
+    cliente = input("Ingrese si tiene membresia si/no: ").lower() == "no"
 
-if(es_vip):
-    descuento = subtotal*0.10
-    print("Tiene acceso a descuento") 
-else:
-    print("No tiene acceso a descuento")
 
-total = subtotal - descuento
 
-print("-"*36)
-print("    Resumen de la compra   ")
-print("-"*36)
+    es_vip = bool()
 
-print(nombre, apellido)
-print(subtotal)
-if(cliente == "si"):
-    print(descuento)
-print(total)
+    if cliente == "si":
+        es_vip=True
+    else:
+        es_vip=False    
+
+
+    subtotal = precio * productos
+
+    if(es_vip):
+        descuento = subtotal*0.10
+        print("Tiene acceso a descuento") 
+    else:
+        descuento = 0
+        print("No tiene acceso a descuento")
+
+    total = subtotal - descuento
+
+    print("-"*36)
+    print("    Resumen de la compra   ")
+    print("-"*36)
+
+    print(nombre, apellido)
+    print(subtotal)
+    if(cliente == "si"):
+        print(descuento)
+    print(total)
+except:
+    print("Error: Ingrese ")
